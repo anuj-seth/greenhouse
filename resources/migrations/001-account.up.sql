1023fba69362d22e6114374a31215cd27b6bdfb9
Anuj Seth <anujseth76@gmail.com>
2021-10-23 06:32:38 +0530

    document the purpose of the tables

--- resources/migrations/001-account.up.sql
+++ resources/migrations/001-account.up.sql
@@ -1,8 +1,18 @@
+-- this is the primary account information table.
+-- as a rule every activity on an account must lock the
+-- row in this table before performing any activity on an account
+-- even if that activity does not actually update data in this table.
 create table bank.accounts (
   account_id bigserial primary key,
   account_name text
 );
 
+-- the opening balance is calculated at the start of every month
+-- and is the sum total of all credits and debits recorded in the account_transactions
+-- table after the last opening balance was computed.
+-- note that this is not actual account balance at any given point in time.
+-- for an updated account balance use the `amount` from this table and add/subtract all the
+-- transactions that come after that `balance_date`
 create table bank.account_opening_balances (
   account_id bigint not null references bank.accounts(account_id),
   balance_date date not null,
@@ -10,6 +20,8 @@ create table bank.account_opening_balances (
   primary key (account_id, balance_date)
 );
 
+-- reference table for all the types of transactions the
+-- system supports
 create table bank.transaction_types (
   transaction_type text primary key);
 
@@ -17,8 +29,13 @@ insert into bank.transaction_types
 values ('credit'),
        ('debit');
 
+-- all the credits and debits against an account are logged in this table.
 create table bank.account_transactions (
   transaction_id bigserial primary key,
   account_id bigint not null references bank.accounts(account_id),
-  transaction_type text not null references bank.transaction_types(transaction_type) 
+  transaction_type text not null references bank.transaction_types(transaction_type),
+  amount float not null,
+  transaction_date timestamptz not null default now()
 );
+
+create index bank_account_transactions_account_id_idx on bank.account_transactions(account_id);
--- resources/migrations/002-fund_transfers.up.sql
+++ resources/migrations/002-fund_transfers.up.sql
@@ -1,8 +1,13 @@
+-- a fund transfer consists of a debit from the source and a credit to the target account,
+-- both of which are stored in the `bank.account_transactions` table.
+-- this table links the two legs of the fund transfer
 create table bank.fund_transfers (
-  credit_transaction_id bigint not null references bank.account_transactions(transaction_id),
   debit_transaction_id bigint not null references bank.account_transactions(transaction_id),
+  credit_transaction_id bigint not null references bank.account_transactions(transaction_id),
   from_account bigint not null references bank.accounts(account_id),
   to_account bigint not null references bank.accounts(account_id),
   transaction_date timestamptz not null,
   primary key (debit_transaction_id, credit_transaction_id)
 );
+
+create unique index bank_fund_transfers_credit_transaction_id_uidx on bank.fund_transfers(credit_transaction_id);
