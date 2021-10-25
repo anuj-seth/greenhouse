4194136a8603d8937147125485e386a49198157e
Anuj Seth <anujseth76@gmail.com>
2021-10-26 00:12:14 +0530

    support running from jar

--- src/greenhouse/core.clj
+++ src/greenhouse/core.clj
@@ -1,7 +1,22 @@
 (ns greenhouse.core
+  (:require [clojure.tools.logging :as logging]
+            [integrant.core :as ig]
+            [greenhouse.system :as sys])
   (:gen-class))
 
+(def system nil)
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (logging/debug "Starting system")
+  (alter-var-root #'system
+                  (fn [_]
+                    (ig/init (sys/new-system))))
+  (logging/info "System started")
+  (.addShutdownHook (Runtime/getRuntime)
+                    (Thread. (fn []
+                               (logging/info "Stopping system")
+                               (ig/halt! system)
+                               (logging/info "Stopped system"))))
+  ;; All threads are daemon, so block forever:
+  @(promise))
